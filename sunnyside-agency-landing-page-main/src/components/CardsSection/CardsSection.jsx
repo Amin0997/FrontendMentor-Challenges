5a8c577edf8a382da44dd69df5d800ad6f6ed005
unknown <amin.khachirov@gmail.com>
2023-08-06 19:42:20 +0300

    123

--- sunnyside-agency-landing-page-main/src/components/CardsSection/CardsSection.jsx
+++ sunnyside-agency-landing-page-main/src/components/CardsSection/CardsSection.jsx
@@ -8,7 +8,7 @@ function CardsSection() {
             <div className='first-box text-box'>
                 <h3 className='text-box__title'>Transform your brand</h3>
                 <p className='text-box__description'>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-                <a href="#" className='first-box__learn-more_btn learn-more_btn'>LEARN MORE</a>
+                <a href="##" className='first-box__learn-more_btn learn-more_btn'>LEARN MORE</a>
             </div>
 
             <div className='second-box img_box'>
@@ -22,7 +22,7 @@ function CardsSection() {
             <div className='fourth-box text-box'>
                 <h3 className='text-box__title'>Stand out to the right audience</h3>
                 <p className='text-box__description'>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. </p>
-                <a href="#" className='fourth-box__learn-more_btn learn-more_btn'>LEARN MORE</a>
+                <a href="##" className='fourth-box__learn-more_btn learn-more_btn'>LEARN MORE</a>
             </div>
 
             <div className='fifth-box img_box text-and-img'>
